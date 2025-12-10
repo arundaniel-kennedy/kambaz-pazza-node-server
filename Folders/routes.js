1a051c7a9156c6710e575b5ec70fbdafb86d9219
Arun Daniel <arundanielkennedy@gmail.com>
2025-12-10 12:07:28 -0500

    updated schema for post

--- Folders/routes.js
+++ Folders/routes.js
@@ -21,13 +21,28 @@ export default function PazzaFolderRoutes(app) {
             })
         }
     }
+    const updateFolderName = async (req, res) => {
+        const { folderId } = req.params
+        const folderName = req.body.name
+        try {
+            const status = await dao.updateFolderName(folderId, folderName)
+            res.json(status)
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({
+                "status": "failed",
+                "message": e.errorResponse
+            })
+        }
+    }
     const deleteFolders = async (req, res) => {
         const { courseId } = req.params
         const folderNames = req.body
         const status = await dao.deleteFolders(courseId, folderNames)
         res.sendStatus(200)
     }
-    app.post("/api/pazza/:courseId/folders/delete", deleteFolders)
+    app.put("/api/pazza/:courseId/folders/:folderId", updateFolderName)
+    app.put("/api/pazza/:courseId/folders/delete", deleteFolders)
     app.post("/api/pazza/:courseId/folders", createFolderForCourse)
     app.get("/api/pazza/:courseId/folders", findFoldersForCourse)
 };
--- Posts/dao.js
+++ Posts/dao.js
@@ -4,7 +4,11 @@ import UserModel from "../../Kambaz/Users/model.js";
 import AnswerModel from "../Answers/model.js";
 export default function PostsDao() {
   async function getAllPostsForCourse(courseId) {
-    return await model.find({ course: courseId }).sort({ timestamp: -1 }).populate("author");
+    return await model
+      .find({ course: courseId })
+      .sort({ timestamp: -1 })
+      .populate("author")
+      .populate("folder")
   }
 
   async function getTodayPosts(courseId) {
@@ -127,13 +131,13 @@ export default function PostsDao() {
   }
 
   //edit a post
-  async function editPost(postId, postUpdates,userId) {
+  async function editPost(postId, postUpdates, userId) {
     const oldPost = await model.findById(postId).populate("author");
-    if(!oldPost) {
+    if (!oldPost) {
       throw new Error("Post not found");
     }
     const isInstr = oldPost.author.role === "FACULTY";
-    if(!isInstr || oldPost.author._id !== userId) {
+    if (!isInstr || oldPost.author._id !== userId) {
       throw new Error("Not authorized to edit this post");
     }
     Object.assign(oldPost, postUpdates);
@@ -148,8 +152,9 @@ export default function PostsDao() {
     const post = await model
       .findById(postId)
       .populate("author")
-      .populate("answer");
-      return post;
+      .populate({ path: 'student_answer.author', model: UserModel })
+      .populate({ path: 'instructor_answer.author', model: UserModel })
+    return post;
   }
 
   //record a view
